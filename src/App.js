27362b824e6865ac4143be88a9007548b13b80b9
Beaumont Yun <b.yun@hotmail.com>
2022-06-01 15:54:07 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+import "./App.scss"
+// NOTE: useFrame has to be used in its own component.
+import { Canvas, useFrame } from "@react-three/fiber"
+
+// import { Box } from "@react-three/drei"
+
+const SpinningMesh = ({position, args, color}) => {
+  const mesh = useRef(null)
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
+
+  return (
+    <mesh position={position} ref={mesh}>
+      {/* args always passed in an array: height, width, depth (i.e. X, Y, Z) */}
+      <boxBufferGeometry attach='geometry' args={args} />
+      <meshStandardMaterial attach='material' color={color} />
+    </mesh>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Canvas camera={{position: [-5, 2, 10], fov: 30}}>
+        {/* ambient light never has shadows */}
+        <ambientLight intensity={.1} />
+        <pointLight position={[-10, 0, -20]} intensity={.5} />
+        <pointLight position={[0, -10, 0]} intensity={1.5} />
+        <SpinningMesh position={[0, 1, 0]} args={[3, 2, 1]} color='lightblue'/>
+        <SpinningMesh position={[-2, 1, -5]} color='pink' />
+        <SpinningMesh position={[5, 1, -2]} colo='green' />
+      </Canvas>
+    </>
   );
 }
 
